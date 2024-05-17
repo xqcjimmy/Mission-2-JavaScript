1a961ce9def5a30f621c0a4ba724ae1bca458c97
Hanbin Choi <hanbinchoi12@gmail.com>
2024-05-17 23:20:07 +1200

    added first function for javascript, added images and borders, minimal styling for reference

--- Mission2script.js
+++ Mission2script.js
@@ -1,1 +1,9 @@
 console.log("Mission 2 is Connected 🕹️");
+
+// Play BTN
+$(document).ready(function(){
+    $('#play').on('click',function(){
+        $('#Video1')[0].src += "&autoplay=1";
+    });
+}); 
+
